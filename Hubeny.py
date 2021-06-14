77ddb03dbdb383e01450faf05f75fa07097b05a2
Shin Maeyamada <66863992+smaeyamada@users.noreply.github.com>
2021-06-14 11:31:16 +0900

    Update Hubeny.py

--- Hubeny.py
+++ Hubeny.py
@@ -15,3 +15,23 @@ def Hubeny(lat0, lon0, lat1, lon1):
     N = Rx / W                  # 卯酉線曲率半径
     D = np.sqrt(((Dy*M)**2) + ((Dx*N*np.cos(P))**2))
     return D
+
+
+POLE_RADIUS = 6356752    # 極半径(短半径)
+EQUATOR_RADIUS = 6378137 # 赤道半径(長半径)
+E = 0.081819191042815790 # 離心率
+E2= 0.006694380022900788 # 離心率の２乗
+
+def Hubeny2(lat0, lon0, lat1, lon1):
+    a_rad_lat = np.radians(lat0)
+    a_rad_lon = np.radians(lon0)
+    b_rad_lat = np.radians(lat1)
+    b_rad_lon = np.radians(lon1)
+    m_lat = (a_rad_lat + b_rad_lat) / 2 # 平均緯度
+    d_lat = a_rad_lat - b_rad_lat # 緯度差
+    d_lon = a_rad_lon - b_rad_lon # 経度差
+    W = np.sqrt(1 - E2 * np.power(np.sin(m_lat), 2))
+    M = EQUATOR_RADIUS * (1 - E2) / np.power(W, 3) # 子午線曲率半径
+    N = EQUATOR_RADIUS / W # 卯酉線曲率半径
+    d = np.sqrt(np.power(M * d_lat, 2) + np.power(N * d_lon * np.cos(m_lat), 2))
+    return d
